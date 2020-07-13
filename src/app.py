6eaa3941410e0c655c96e0a490fbb86020df5586
valentin <jijkoun@gmail.com>
2020-07-13 12:16:03 +0200

    Added frontend page

--- src/app.py
+++ src/app.py
@@ -1,30 +1,48 @@
 """AWS Lambda implementation for the Sudoku solver"""
 import urllib.request
+import copy
+import json
 
 import image
 import grid
 import digit
+import solver
 
 
 digit.initialize_predictor()
 
 
 def lambda_handler(event, context):
-    url = event['queryStringParameters'].get('url')
-    if not url:
+    try:
+        body = json.loads(event['body'])
+        url = body['url']
+    except Exception as e:
         return {
             'statusCode': 400,
-            'body': 'Missing url parameter'
+            'body': 'Cannot get url from request: %s' % e,
         }
-    req = urllib.request.urlopen(url)
-    img, bitmap = image.read_image(req)
+    img_data = urllib.request.urlopen(url)
+
+    img, bitmap = image.read_image(img_data)
     cells = grid.detect_filled_cells(bitmap)
     sudoku = [[0 for _ in range(9)] for _ in range(9)]
     for cell in cells:
         value = digit.predict(cell[0])
         pos = cell[2]
-        sudoku[pos[0]][pos[1]] = value
+        sudoku[pos[0]][pos[1]] = int(value)
+    result = copy.deepcopy(sudoku)
+    if not solver.solve(result):
+        result = None
+
     return {
         'statusCode': 200,
-        'body': str(sudoku)
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST',
+        },
+        'body': json.dumps({
+            'input': sudoku,
+            'solved': result,
+        }),
     }
